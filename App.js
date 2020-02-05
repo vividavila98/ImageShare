d4978297bf680fe9fc388b089a998a2e3bf5469b
Viviana Davila <vdavila@ucsd.edu>
2020-02-05 13:08:13 -0800

    Image Picker

--- App.js
+++ App.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
-//import logo from './assets/logo.png';
+import { Image, StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import logo from './assets/ImageShare.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  // hooks
+  let [selectedImage, setSelectedImage] = React.useState(null);
+
+  // using expo-image-picker library, async request!
+  let openImagePickerAsync = async () => {
+    let permissionResult = 
+    await ImagePicker.requestCameraRollPermissionsAsync();
+    
+    if(permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    //console.log(pickerResult);
+
+    if(pickerResult.cancelled) {
+      return;
+    }
+
+    setSelectedImage({localUri: pickerResult.uri});
+  };
+
+  if(selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }} 
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={{uri: 'https://en.instagram-brand.com/wp-content/uploads/2016/11/Glyph-Icon-hero.png'}} style={styles.logo}/>
-      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below.</Text>
+      <Image source={logo} style={styles.logo}/>
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, 
+        just press the button below!
+      </Text>
+      {/* Button pressed to pick picture */}
+      <TouchableOpacity 
+        onPress={openImagePickerAsync}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -19,14 +63,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   logo: {
-    width: 305, 
-    height: 159, 
-    marginBottom: 15
+    width: 250, 
+    height: 250, 
   },
   instructions: {
     color: '#888', 
     fontSize: 20, 
     textAlign: "center",
-    marginHorizontal: 10 
+    marginHorizontal: 10,
+    marginVertical: 15
+  },
+  button: {
+    backgroundColor: '#1647fb',
+    padding: 17,
+    borderRadius: 6
+  },
+  buttonText: { 
+    fontSize: 20, 
+    color: '#fff' 
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'cover',
   },
 });
